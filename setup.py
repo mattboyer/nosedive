024490dd5aa8e636f59d8b5a958c2d59c99c701e
Matt Boyer <mboyer@sdf.org>
2015-12-06 22:15:04 +0000

    Very early release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='nosedive',
     version=version.get_git_version(),
     url='https://github.com/mattboyer/',
-    description=('Hurr Durr'),
+    description=('Nosedive is a stack depth tracer for Nose'),
     author='Matt Boyer',
     author_email='mboyer@sdf.org',
     license='BSD',
@@ -16,6 +16,7 @@ setup(
     keywords='nosetests',
     packages=['NoseDive'],
     install_requires=['nose'],
-    entry_points = {
-        'nose.plugins.0.10': [ 'krunt = NoseDive.depth:DepthPlugin' ] },
+    entry_points={
+        'nose.plugins.0.10': ['nosedive = NoseDive.depth:DepthPlugin']
+    },
 )
